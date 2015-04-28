1f9883f0a0630edb1d21e7d21d0f9ef4e260fee8
Cody Sehl <csehl@knox.edu>
2015-04-28 15:00:35 -0500

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def authenticate_with_token_and_set_user(options = {})
+  	before_action(options) do
+  		@user = User.find_by_token(params[:token])
+  		if !@user.present?
+  			
+  		end
+  	end
+  end
 end
