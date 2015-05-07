871a8e48803bd70701ccf46397f09485c30eaef9
Cody Sehl <csehl@knox.edu>
2015-05-07 08:27:16 -0500

    [#93988456] Making a request to /sessions without a course_id should return all sessions for a user, not just a course

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < User
 	has_and_belongs_to_many :courses, foreign_key: :user_id
+	has_many :sessions, :through => :courses
 	has_many 				:mc_responses
 	has_many 				:sa_responses
 	has_many 				:num_responses
